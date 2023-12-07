346e52db82e9ce589d51c2d29eb616aa16ae4264
rizky-gumelar <111202113304@mhs.dinus.ac.id>
2023-12-08 00:02:16 +0700

    my first game project

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1641181280,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground7.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"media/bgmusic.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
